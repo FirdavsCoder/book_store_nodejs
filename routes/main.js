9813c37592a8868563b2b8f916658bc9ec0e2976
FirdavsCoder <firdavsyorkulov@gmail.com>
2023-11-17 02:43:28 +0500

    updated

--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router()
 const path = require("path")
 
-const users = require("./users")
+const {users} = require("./users")
+const { log } = require("console")
 
 router.get("/", (req, res) => {
-    res.render("main.pug")
+    console.log(users);
+    res.render("news.pug", {title: "Hamma kitoblar", users})
 })
 
 router.get("/subpage", (req, res) => {
